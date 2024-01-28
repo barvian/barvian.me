de666ca514cea65625a218c688c53439891fa523
Maxwell Barvian <max@barvian.me>
2024-01-28 00:04:57 -0600

    Fixes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -31,7 +31,8 @@ export default {
 				DEFAULT: 'cubic-bezier(.165,.84,.44,1)'
 			},
 			transitionDuration: {
-				DEFAULT: '400ms'
+				DEFAULT: '400ms',
+				450: '450ms'
 			},
 			backgroundImage: {
 				'to-dark': 'linear-gradient(to bottom, transparent 0%, transparent 8.1%, rgba(0,0,0,0.001) 15.5%, rgba(0,0,0,0.003) 22.5%, rgba(0,0,0,0.005) 29%, rgba(0,0,0,0.008) 35.3%, rgba(0,0,0,0.011) 41.2%, rgba(0,0,0,0.014) 47.1%, rgba(0,0,0,0.016) 52.9%, rgba(0,0,0,0.019) 58.8%, rgba(0,0,0,0.022) 64.7%, rgba(0,0,0,0.025) 71%, rgba(0,0,0,0.027) 77.5%, rgba(0,0,0,0.029) 84.5%, rgba(0,0,0,0.03) 91.9%, rgba(0,0,0,0.03) 100%)',
