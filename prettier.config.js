62feaedae71904ffb3e196e10207c56834a4b85d
Maxwell Barvian <max@barvian.me>
2024-01-31 23:27:37 -0600

    Init prettier

---
+++ prettier.config.js
@@ -0,0 +1,18 @@
+/** @type {import("prettier").Config} */
+export default {
+    plugins: ['prettier-plugin-astro', 'prettier-plugin-svelte', 'prettier-plugin-tailwindcss'],
+    overrides: [
+      {
+        files: '*.astro',
+        options: {
+          parser: 'astro',
+        },
+      },
+      {
+        files: '*.svelte',
+        options: {
+          parser: 'svelte',
+        },
+      },
+    ],
+  };
