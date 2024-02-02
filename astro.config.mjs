47cd87b2a51553064856853f3580a08daac6cbbf
Maxwell Barvian <max@barvian.me>
2024-02-02 06:12:25 -0600

    Add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import svelte from '@astrojs/svelte'
 import vue from '@astrojs/vue'
-
 import mdx from '@astrojs/mdx'
+import vercel from '@astrojs/vercel/static'
 
 // https://astro.build/config
 export default defineConfig({
@@ -20,5 +20,11 @@ export default defineConfig({
 		}),
 		mdx()
 	],
-	srcDir: './'
+	srcDir: './',
+	output: 'static',
+	adapter: vercel({
+		webAnalytics: {
+			enabled: true
+		}
+	})
 })
