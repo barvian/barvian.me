eea85f4fdbccdba725c1514e236297bbd132d393
Maxwell Barvian <max@barvian.me>
2024-02-04 23:34:40 -0600

    Fix project page flickering

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -53,6 +53,9 @@ export default {
 				DEFAULT: '400ms',
 				450: '450ms'
 			},
+			maxWidth: {
+				prose: '43rem' // the default 65ch seemingly waits for the font-face to load, which causes a FOUC
+			},
 			backgroundImage: {
 				'to-dark':
 					'linear-gradient(to bottom, transparent 0%, transparent 8.1%, rgba(0,0,0,0.001) 15.5%, rgba(0,0,0,0.003) 22.5%, rgba(0,0,0,0.005) 29%, rgba(0,0,0,0.008) 35.3%, rgba(0,0,0,0.011) 41.2%, rgba(0,0,0,0.014) 47.1%, rgba(0,0,0,0.016) 52.9%, rgba(0,0,0,0.019) 58.8%, rgba(0,0,0,0.022) 64.7%, rgba(0,0,0,0.025) 71%, rgba(0,0,0,0.027) 77.5%, rgba(0,0,0,0.029) 84.5%, rgba(0,0,0,0.03) 91.9%, rgba(0,0,0,0.03) 100%)',
