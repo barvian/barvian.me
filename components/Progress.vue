3fcaa2c4c375989a90ff0890fda79b5e8c316174
Maxwell Barvian <max@barvian.me>
2024-03-30 04:23:34 -0500

    Add progress bar (finally)

---
+++ components/Progress.vue
@@ -0,0 +1,55 @@
+<template>
+	<div id="nprogress-container"></div>
+</template>
+
+<script setup lang="ts">
+import { useEventListener } from '@vueuse/core'
+import { isTransitionBeforePreparationEvent } from 'astro:transitions/client'
+import NProgress from 'nprogress'
+import { onMounted } from 'vue'
+
+onMounted(() => {
+	NProgress.configure({
+		parent: '#nprogress-container',
+		showSpinner: false
+	})
+})
+
+if (typeof document !== 'undefined')
+	useEventListener(document, 'astro:before-preparation', (event) => {
+		if (!isTransitionBeforePreparationEvent(event)) return
+
+		NProgress.start()
+	})
+
+if (typeof document !== 'undefined')
+	useEventListener(document, 'astro:after-swap', (event) => {
+		NProgress.done()
+	})
+</script>
+
+<style>
+@import 'nprogress/nprogress.css';
+
+#nprogress .bar {
+	position: static !important;
+}
+
+#nprogress-container {
+	view-transition-name: nprogress;
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+}
+
+::view-transition-old(nprogress) {
+	/* This is a persistent element, so let the new always show on top. See https://developer.chrome.com/docs/web-platform/view-transitions#making_the_most_of_content_you_already_have */
+	animation: none;
+	opacity: 0;
+}
+
+::view-transition-new(nprogress) {
+	animation: none;
+}
+</style>
