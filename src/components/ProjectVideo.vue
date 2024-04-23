2a7c6875077247035a310587bd2ad77652b6095a
Maxwell Barvian <max@barvian.me>
2024-04-23 10:14:23 -0500

    Fix type imports

--- src/components/ProjectVideo.vue
+++ src/components/ProjectVideo.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { type ProjectEntry } from '@/schemas/work'
+import type { ProjectEntry } from '@/schemas/work'
 import useTransitioning from '@/composables/useTransitioning'
 import Video from '@/components/Video.vue'
 
@@ -24,11 +24,11 @@ const hasDemo = !import.meta.env.SSR && window.__PROJECTS_WITH_DEMOS__.includes(
 			(transitioning?.from.pathname === '/' + slug && !hasDemo) ||
 			transitioning?.to.pathname === '/' + slug
 		"
-		:src
-		:poster
 		muted
 		playsinline
 		loop
 		autoplay
+		:src
+		:poster
 	/>
 </template>
