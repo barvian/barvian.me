10044f570eeede323162627a5e27220c64c64904
Maxwell Barvian <max@barvian.me>
2024-04-23 07:35:10 -0500

    Try removing middleware

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-import type { MiddlewareHandler } from 'astro'
-import { url as urlRef } from '/stores/page'
-
-export const onRequest: MiddlewareHandler = ({ url }, next) => {
-	// Expose the request URL for framework components SSR (equivalent to Astro.url in Astro component)
-	urlRef.value = url
-
-	return next()
-}
