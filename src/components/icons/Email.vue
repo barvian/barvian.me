c28ec9ed0e05ff8ae54c8fb3900b3aeb9875484f
Maxwell Barvian <max@barvian.me>
2024-06-12 10:07:57 -0500

    Update Email.vue

--- src/components/icons/Email.vue
+++ src/components/icons/Email.vue
@@ -4,7 +4,7 @@
 		<path
 			d="M21 3C21 1.9 20.1 1 19 1H3C1.9 1 1 1.9 1 3M21 3V15C21 16.1 20.1 17 19 17H3C1.9 17 1 16.1 1 15V3M21 3L11 10L1 3"
 			stroke="currentColor"
-			stroke-width="2"
+			stroke-width="1.75"
 			stroke-linecap="round"
 			stroke-linejoin="round"
 			vector-effect="non-scaling-stroke"
