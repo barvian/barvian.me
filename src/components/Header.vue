e5e9fc0dd5a7deda79f6f74bb64ecd81384428b0
Maxwell Barvian <max@barvian.me>
2024-06-12 23:00:35 -0500

    SEO

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -24,7 +24,7 @@ const isMounted = useMounted()
 				style="view-transition-name: back"
 			/>
 			<span style="view-transition-name: name">Maxwell Barvian</span><br />
-			<span style="view-transition-name: title">Front-end Developer</span>
+			<span style="view-transition-name: title">Design Engineer</span>
 		</h1>
 		<div class="max-sm:relative sm:contents">
 			<div
